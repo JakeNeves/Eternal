946d96e5374267a6326b465a537032929f47e02e
JakeNeves <67692621+JakeNeves@users.noreply.github.com>
2020-07-01 16:36:24 -0300

    Attempting to port the sources on here...

---
+++ Eternal.cs
@@ -0,0 +1,11 @@
+using Terraria.ModLoader;
+
+namespace Eternal
+{
+	public class Eternal : Mod
+	{
+		public Eternal()
+		{
+		}
+	}
+}
---
+++ EternalPlayer.cs
@@ -0,0 +1,24 @@
+﻿using Eternal.Items;
+using Eternal.Items.Accessories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameInput;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+namespace Eternal
+{
+    class EternalPlayer : ModPlayer
+    {
+		public override void SetupStartInventory(IList<Item> items, bool mediumcoreDeath)
+		{
+			Item item = new Item();
+			item.SetDefaults(ItemType<AncientPendant>());
+			item.SetDefaults(ItemType<CreatorsMessage>());
+			items.Add(item);
+		}
+	}
+}
