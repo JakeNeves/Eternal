97548114ddeab8c3a42fc1f98db86326a25ae80a
JakeNeves <67692621+JakeNeves@users.noreply.github.com>
2020-07-08 17:51:25 -0300

    Add files via upload

---
+++ Eternal.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Eternal
+{
+	public class Eternal : Mod
+	{
+		public Eternal()
+		{
+
+			Properties = new ModProperties()
+			{
+				Autoload = true,
+				AutoloadGores = true,
+				AutoloadSounds = true,
+				AutoloadBackgrounds = true
+			};
+
+		}
+
+		public override void UpdateMusic(ref int music, ref MusicPriority priority)
+        {
+			if(Main.myPlayer == -1 || Main.gameMenu || !Main.LocalPlayer.active)
+            {
+				return;
+            }
+
+			if(Main.LocalPlayer.GetModPlayer<EternalPlayer>().ZoneThunderduneBiome)
+            {
+				music = GetSoundSlot(SoundType.Music, "Sounds/Music/VitreousSandsofThunder");
+				priority = MusicPriority.BiomeLow;
+            }
+
+        }
+
+	}
+}
