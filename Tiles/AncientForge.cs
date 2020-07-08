dc3a064dfcc4c54db744a67ef381df7976923a2a
JakeNeves <67692621+JakeNeves@users.noreply.github.com>
2020-07-08 17:49:24 -0300

    Add files via upload

---
+++ Tiles/AncientForge.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Eternal.Tiles
+{
+    class AncientForge : ModTile
+    {
+
+        public override void SetDefaults()
+        {
+            Main.tileFrameImportant[Type] = true;
+            Main.tileNoAttach[Type] = true;
+            Main.tileTable[Type] = true;
+            Main.tileLavaDeath[Type] = false;
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x3);
+            TileObjectData.addTile(Type);
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Ancient Forge");
+            AddMapEntry(new Color(80, 170, 195), name);
+            disableSmartCursor = true;
+            //The List of Tiles it Functions As
+            adjTiles = new int[]
+            {
+                TileID.WorkBenches,
+                TileID.AdamantiteForge,
+                TileID.MythrilAnvil,
+                TileID.Bottles,
+                TileID.AlchemyTable,
+                TileID.CookingPots,
+                TileID.HeavyWorkBench,
+                TileID.Blendomatic,
+                TileID.LunarCraftingStation,
+                TileID.Sawmill,
+                TileID.Loom,
+                TileID.Kegs
+            };
+        }
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(i * 16, j * 16, 32, 16, ItemType<Items.Placeable.AncientForge>());
+        }
+
+    }
+}
---
+++ Tiles/LabrynthStone.cs
@@ -0,0 +1,29 @@
+﻿using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+
+namespace Eternal.Items.Placeable
+{
+    class LabrynthStone : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Mined with a Lunar Pickaxe or Higher...");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 16;
+            item.height = 16;
+            item.maxStack = 999;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.rare = ItemRarityID.Green;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.createTile = TileType<Tiles.LabrynthStone>();
+        }
+    }
+}
