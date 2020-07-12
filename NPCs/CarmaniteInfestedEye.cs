cdb82e3504940bb97901fa88fd5b8879e985fcad
JakeNeves <67692621+JakeNeves@users.noreply.github.com>
2020-07-12 14:36:25 -0300

    Add files via upload

---
+++ NPCs/CarmaniteInfestedEye.cs
@@ -0,0 +1,48 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Eternal.Items.Summon;
+using static Terraria.ModLoader.ModContent;
+using Eternal.Items;
+
+namespace Eternal.NPCs
+{
+    class CarmaniteInfestedEye : ModNPC
+    {
+
+        public override void SetStaticDefaults()
+        {
+            Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.DemonEye];
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width = 18;
+            npc.height = 24;
+            npc.damage = 2;
+            npc.defense = 2;
+            npc.lifeMax = 15;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.value = 50f;
+            npc.aiStyle = 2;
+            aiType = NPCID.DemonEye;
+            animationType = NPCID.DemonEye;
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.OverworldNightMonster.Chance * 0.5f;
+        }
+
+        public override void NPCLoot()
+        {
+            if (Main.rand.Next(10) == 0)
+            {
+                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemType<SuspiciousLookingDroplet>());
+            }
+            Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemType<Carmanite>(), Main.rand.Next(5, 20));
+        }
+
+    }
+}
