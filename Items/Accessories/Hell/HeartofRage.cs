4e38e464755e81c591525d1e4632eabe97a26fc7
JakeNeves <67692621+JakeNeves@users.noreply.github.com>
2020-07-25 10:53:55 -0300

    Add files via upload

---
+++ Items/Accessories/Hell/HeartofRage.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Eternal.Items.Accessories.Hell
+{
+    class HeartofRage : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Heart of Rage");
+            Tooltip.SetDefault("Increased Melee Damage By 5%\n[c/B50515:Hell]");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 26;
+            item.value = 0;
+            item.rare = ItemRarityID.Expert;
+            item.accessory = true;
+        }
+
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
+        {
+             tooltips[0].overrideColor = new Color(180, 5, 20);
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.meleeDamage += 5f;
+        }
+    }
+}
